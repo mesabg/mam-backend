bfd718a0d56c0ceecc415beaf9350cbca632c7b3
Prodigy <peeddurple@gmail.com>
2018-01-14 23:55:37 -0400

    Fixing Question.js

--- models/Question.js
+++ models/Question.js
@@ -6,16 +6,16 @@ var Types = keystone.Field.Types;
  * =============
  */
 
-var Questions = new keystone.List('Question', {
+var Question = new keystone.List('Question', {
 	autokey: { path: 'slug', from: 'name', unique: true },
 });
 
-Questions.add({
+Question.add({
 	name: { type: String, required: true},
 	question: { type: Types.Html, wysiwyg: true, height: 150 },
 	answer: { type: Types.Html, wysiwyg: true, height: 150 },
 });
 
-Questions.relationship({ ref: 'ContactPage', path: 'contact-pages', refPath: 'questions' });
+Question.relationship({ ref: 'ContactPage', path: 'contact-pages', refPath: 'questions' });
 
-Questions.register();
+Question.register();
