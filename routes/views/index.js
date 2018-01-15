6154939e860bd84fb50a84995b7f5f341c0ceab2
Prodigy <peeddurple@gmail.com>
2018-01-14 23:02:57 -0400

    Creating REST Endpoints for Question structure#3

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,5 +1,9 @@
 var keystone = require('keystone');
 
+var routes = {
+    views: importRoutes('./views')
+};
+
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
