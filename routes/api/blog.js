00a20f14b7df06565ddf8d5e223cb57576b121ce
Prodigy <peeddurple@gmail.com>
2018-02-16 00:50:29 -0400

    blog rest endpoint25

--- routes/api/blog.js
+++ routes/api/blog.js
@@ -11,7 +11,7 @@ exports.list = function(req, res) {
         if (err) return res.json({ err: err });
         var a = [];
         for(var i =0;i<items[0].articles.length;i++){
-            a.push(Articles.model.find().where('_id', items[0].articles[i]));
+            a.push(Articles.model.find().where('_id', items[0].articles[i]).exec(function(errpost, article) {return article}));
         }
         res.json({
             Blog: a
