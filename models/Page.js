488d0a69ad84f4fd4e181aa128455f68e2009743
Moisés Berenguer <moises.berenguer@gmail.com>
2018-04-13 19:33:03 -0400

    Adding nodelete parameter to Page creation

--- models/Page.js
+++ models/Page.js
@@ -2,27 +2,34 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 /**
- * Page Model
- * ==========
+ * Page
+ * @param {String} title
+ * @param {String} state
+ * @param {Date} publishedDate
+ * @param {CloudinaryImage} banner
+ * @param {title, description} content
+ * ==================================
  */
+
 var Page = new keystone.List('Page', {
-  map: { name: 'title' },
-  autokey: { path: 'slug', from: 'title', unique: true },
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true },
+	nodelete: true
 });
 
 Page.add({
-  title: { type: String, required: true },
-  state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-  publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-  banner: { type: Types.CloudinaryImage },
-  content: {
-    title: { type: Types.Html, wysiwyg: true, height: 150 },
-    description: { type: Types.Html, wysiwyg: true, height: 400 },
-  }
+	title: { type: String, required: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	banner: { type: Types.CloudinaryImage },
+	content: {
+		title: { type: Types.Html, wysiwyg: true, height: 150 },
+		description: { type: Types.Html, wysiwyg: true, height: 400 },
+	}
 });
 
 Page.schema.virtual('content.full').get(function () {
-  return this.content.extended || this.content.brief;
+	return this.content.extended || this.content.brief;
 });
 
 Page.defaultColumns = 'title, state|20%';
