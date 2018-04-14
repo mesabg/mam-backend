4ecf41c07d736ec50ab446615a7c440f3a3ead0c
Moisés Berenguer <moises.berenguer@gmail.com>
2018-04-13 23:38:09 -0400

    Documentation with apidoc added

---
+++ routes/api/documentation/general/error.js
@@ -0,0 +1,11 @@
+/**
+ * Common Error
+ * @apiDefine CommonError
+ * @apiErrorExample Common Error:
+ * HTTP/1.1 200 Ok
+{
+    "statusMessage": "error",
+    "statusCode": 500,
+    "data": null
+}
+ */
---
+++ routes/api/documentation/pages/404/404.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @api {get} /api/page/404 404 Page
+ * @apiVersion 1.0.0
+ *
+ * @apiName 404
+ * @apiGroup Pages
+ *
+ * @apiParamExample query example:
+ * https://mam-restful-api.herokuapp.com/api/page/404
+ *
+ * @apiUse HeadersRequired
+ * @apiUse SuccessGet404Page
+ * @apiUse CommonError
+ */
