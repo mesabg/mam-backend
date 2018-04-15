d4274e349a0538943ab52298c980fa9d4510b466
Moisés Berenguer <moises.berenguer@gmail.com>
2018-04-14 22:28:38 -0400

    CORS allowed

--- routes/middleware.js
+++ routes/middleware.js
@@ -60,9 +60,11 @@ exports.requireUser = function (req, res, next) {
 
 exports.enableCors = function (request , response , next){
 	request.header("Access-Control-Allow-Origin", "*");
-	request.header("Access-Control-Allow-Headers", "*");
+	response.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+	request.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-mam-api-token");
 	response.header("Access-Control-Allow-Origin", "*");
-	response.header("Access-Control-Allow-Headers", "*");
+	response.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-mam-api-token");
 	next();
 };
 
